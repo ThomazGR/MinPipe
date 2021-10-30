27cb0d8e2fd30293443b4532244cf78571e5647d
Thomaz Guadagnini <thomaz@vivaldi.net>
2021-10-29 21:19:05 -0300

    Adding verification for conda environment and bash shebang

--- src/conda.sh
+++ src/conda.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 # Download Miniconda from Anaconda repo
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
--- src/packages.sh
+++ src/packages.sh
@@ -1,12 +1,23 @@
-# Append new channel to download conda packages
-conda config --add channels conda-forge
-conda config --add channels defaults
-conda config --add channels r
-conda config --add channels bioconda
+#!/usr/bin/bash
 
-# Download all packages needed to run RNA-seq workflow (star not included)
-conda install -c anaconda git wget --yes
-conda install -c bioconda fastqc trim-galore cutadapt kallisto picard --yes
+ENV=`echo $CONDA_DEFAULT_ENV`
 
-echo "All packages have been installed from Bioconda"
-echo "Packages included: fastqc, trim galore, cutadapt, kallisto and picard."
+if [ "$ENV" == "" ]; then
+    echo "You do not have a conda environment active, please activate it running conda activate."
+    exit
+else 
+    # Append new channel to download conda packages
+    conda config --add channels conda-forge
+    conda config --add channels defaults
+    #conda config --add channels r
+    conda config --add channels bioconda
+
+    # Download all packages needed to run RNA-seq workflow (star not included)
+    conda install -c anaconda git wget --yes
+    conda install -c bioconda fastqc trim-galore cutadapt kallisto picard --yes
+
+    echo "All packages have been installed from Bioconda"
+    echo "Packages included: fastqc, trim galore, cutadapt, kallisto and picard."
+    exit
+fi;
+
