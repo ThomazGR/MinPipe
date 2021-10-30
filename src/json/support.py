853935873335523ebe978561df3bf35f82329973
Thomaz Guadagnini <thomaz@vivaldi.net>
2021-10-30 11:46:08 -0300

    Fixing json support and creating yaml support

--- src/json/support.py
+++ src/json/support.py
@@ -1,23 +1,27 @@
 import json
-import argparse
-from itertools import chain
 
 def json_parse(file: str = None) -> list:
     try:
-        with open(f"examples/{file}.json") as fd:
+        with open(f"input/{file}") as fd:
             data = json.load(fd)
     except FileNotFoundError as fnfe:
         raise Exception(fnfe)
 
-    vals = []
-    for k in data.keys():
-        vals.append(f"--{str(k)}")
-        if type(data[k]) == list:
-            for i in data[k]:
-                vals.append(str(i))
-        elif type(data[k]) == bool:
-            pass
-        else:
-            vals.append(str(data[k]))
+    try:
+        vals = []
+        for k in data.keys():
+            if type(data[k]) == bool and data[k] is True:
+                vals.append(f"--{str(k)}")
+            elif type(data[k]) == bool and data[k] is False:
+                pass
+            elif type(data[k]) == list:
+                vals.append(f"--{str(k)}")
+                for i in data[k]:
+                    vals.append(str(i))
+            else:
+                vals.append(f"--{str(k)}")
+                vals.append(str(data[k]))
+    except Exception as exc:
+        raise Exception(exc)
 
     return vals
