493cab0badd998e34cea99bd1d6214eea94518c1
Thomaz Guadagnini <59666076+ThomazGR@users.noreply.github.com>
2022-04-17 18:39:05 -0300

    Formatting all .py files
    Using `black` to format all .py files and follow standards.

--- minpipe/libinst.py
+++ minpipe/libinst.py
@@ -9,25 +9,34 @@ class CheckLibs:
 
     def __check_kallisto(self) -> None:
         kall = run(["which", "kallisto"])
-        if all(contains in str(kall.stdout) for contains in ["kallisto", "/"]) and Path(str(kall.stdout)).is_dir():
+        if (
+            all(contains in str(kall.stdout) for contains in ["kallisto", "/"])
+            and Path(str(kall.stdout)).is_dir()
+        ):
             self.logger.info("Contains Kallisto? Yes")
         else:
             self.logger.info("Contains Kallisto? No")
             self.logger.info("EXITING")
             exit()
-    
+
     def __check_trim_galore(self) -> None:
         tg = run(["which", "trim_galore"])
-        if all(contains in str(tg.stdout) for contains in ["trim_galore", "/"]) and Path(str(tg.stdout)).is_dir():
+        if (
+            all(contains in str(tg.stdout) for contains in ["trim_galore", "/"])
+            and Path(str(tg.stdout)).is_dir()
+        ):
             self.logger.info("Contains Trim Galore? Yes")
         else:
             self.logger.info("Contains Trim Galore? No")
             self.logger.info("EXITING")
             exit()
-    
+
     def __check_fastqc(self) -> None:
         fqc = run(["which", "fastqc"])
-        if all(contains in str(fqc.stdout) for contains in ["fastqc", "/"]) and Path(str(fqc.stdout)).is_dir():
+        if (
+            all(contains in str(fqc.stdout) for contains in ["fastqc", "/"])
+            and Path(str(fqc.stdout)).is_dir()
+        ):
             self.logger.info("Contains FastQC? Yes")
         else:
             self.logger.info("Contains FastQC? No")
